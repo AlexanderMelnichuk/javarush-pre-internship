0ede752812eba87d7bb94cfd75bd5573822c5c4d
Alexander Melnichuk <alexander.melnichuk@aurea.com>
2018-05-02 17:36:04 +1000

    Specified SpringRunner.class instead of SpringJUnit4ClassRunner.class (actually, they are the same, SpringRunner is an alias).

--- src/test/java/ru/ama0/book/BaseTest.java
+++ src/test/java/ru/ama0/book/BaseTest.java
@@ -10,13 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import ru.ama0.book.entity.Book;
 import ru.ama0.book.entity.Chapter;
 import ru.ama0.book.repository.BookRepository;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {JpaConfiguration.class})
 @ActiveProfiles("unittest")
 @Transactional
