5b467a2ba46c4f2d567aad5235e6733e59c061d9
Alexander Melnichuk <alexander.melnichuk@aurea.com>
2018-05-02 11:53:45 +1000

    Simplified Book entity declaration with Lombok annotations

--- src/main/java/ru/ama0/book/entity/Book.java
+++ src/main/java/ru/ama0/book/entity/Book.java
@@ -3,9 +3,13 @@ package ru.ama0.book.entity;
 import java.util.Calendar;
 
 import javax.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
 public class Book {
 	@Id
 	@GeneratedValue
@@ -17,9 +21,6 @@ public class Book {
 	private Integer printYear = Calendar.getInstance().get(Calendar.YEAR);
 	private Boolean readAlready = false;
 
-	public Book() {
-	}
-	
 	public Book(String title, String description, String author, String isbn, Integer printYear) {
 		super();
 		this.title = title;
@@ -30,62 +31,6 @@ public class Book {
 		this.readAlready = false;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public Integer getPrintYear() {
-		return printYear;
-	}
-
-	public void setPrintYear(Integer printYear) {
-		this.printYear = printYear;
-	}
-
-	public Boolean getReadAlready() {
-		return readAlready;
-	}
-
-	public void setReadAlready(Boolean readAlready) {
-		this.readAlready = readAlready;
-	}
-	
 	public void flipReadAlready() {
 		this.readAlready = !this.readAlready;
 	}
